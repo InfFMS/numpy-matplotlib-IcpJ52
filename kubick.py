3beb502938f4f0429da9fffd73479886eb5b507c
IcpJ42 <iblion08@gmail.com>
2025-02-03 12:47:52 +0700

    Переделал задание так, чтобы гистограммы были в одном окне

--- kubick.py
+++ kubick.py
@@ -21,15 +21,17 @@ for el in result:
         max_cnt[elem - 1] = max(max_cnt[elem - 1], cnt)
         elem = el
         cnt = 1
-
+plt.subplot2grid((2, 2), (0, 0))
 plt.bar(categories, values, color='purple')
 plt.xlabel('Результат броска')
 plt.ylabel('Количество выпадений')
-plt.show()
+# plt.show()
+plt.subplot2grid((2, 2), (1, 0))
 plt.bar(categories, chances, color='red')
 plt.xlabel('Результат броска')
 plt.ylabel('Вероятность выпадения, %')
-plt.show()
+# plt.show()
+plt.subplot2grid((2, 2), (0, 1), rowspan=2)
 plt.bar(categories, max_cnt, color='green')
 plt.xlabel('Результат броска')
 plt.ylabel('Максимальное количество подряд выпавших одинаковых значений')
